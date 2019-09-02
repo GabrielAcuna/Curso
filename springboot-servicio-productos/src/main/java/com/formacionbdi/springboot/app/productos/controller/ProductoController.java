594693c1c057076deea2d6b1650286776c97fedc
gabriel.acuna <gabriel.acuna@sistemassa.cl>
2019-09-02 11:37:27 -0400

    commit fuentes

---
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controller/ProductoController.java
@@ -0,0 +1,71 @@
+package com.formacionbdi.springboot.app.productos.controller;
+
+import com.formacionbdi.springboot.app.productos.models.entity.Producto;
+import com.formacionbdi.springboot.app.productos.models.service.IProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class ProductoController {
+
+    @Autowired
+    private Environment env;
+
+    @Value("${server.port}")
+    private Integer port;
+
+    @Autowired
+    private IProductoService productoService;
+
+    @GetMapping("/listar")
+    public List<Producto> listar() {
+        return productoService.findAll().stream().map(producto -> {
+            //producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+            producto.setPort(port);
+            return producto;
+                }).collect(Collectors.toList());
+    }
+
+    @GetMapping("/ver/{id}")
+    public Producto detalle(@PathVariable Long id) throws Exception {
+        Producto producto = productoService.findById(id);
+        //producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+        producto.setPort(port);
+
+        /*try {
+            Thread.sleep(1000L);
+        }catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+        return producto;
+    }
+    @PostMapping("/crear")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Producto crear(@RequestBody Producto producto) {
+        return productoService.save(producto);
+    }
+
+    @PutMapping("/editar/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Producto editar(@RequestBody Producto producto,@PathVariable Long id){
+
+        Producto productoDb = productoService.findById(id);
+        productoDb.setNombre(producto.getNombre());
+        productoDb.setPrecio(producto.getPrecio());
+
+        return productoService.save(productoDb);
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void eliminar(@PathVariable Long id) {
+        productoService.deleteById(id);
+
+    }
+}
